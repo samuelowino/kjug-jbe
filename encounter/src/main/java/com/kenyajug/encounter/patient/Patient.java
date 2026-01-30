5b6075a31cc411b28a7aca4c3e460158535b2744
Samuel Owino <samuelowino43@yahoo.com>
2026-01-30 21:36:41 +0300

    Episode #3

--- encounter/src/main/java/com/kenyajug/encounter/patient/Patient.java
+++ encounter/src/main/java/com/kenyajug/encounter/patient/Patient.java
@@ -1,6 +1,13 @@
 package com.kenyajug.encounter.patient;
+import java.time.LocalDate;
 public record Patient(
         String uuid,
         String name,
-        String dob) {
+        LocalDate dob) {
+    //Cannot be changed using reflection
+    public Patient {
+        if (name.isBlank()) throw new AssertionError("Name cannot be empty");
+        if (dob == null) throw new AssertionError("Dob cannot be null");
+        if (uuid.isBlank()) throw new AssertionError("Id cannot be empty");
+    }
 }
---
+++ encounter/src/test/java/com/kenyajug/encounter/PatientValidatorTest.java
@@ -0,0 +1,31 @@
+package com.kenyajug.encounter;
+import com.kenyajug.encounter.patient.Patient;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+public class PatientValidatorTest {
+    @Test
+    @DisplayName("Check if patient name is blank, otherwise throw")
+    public void throws_ifNameIsBlankTest(){
+        var uuid = "d4d5e81f-c932-4516-81ce-80f2da66ee7f";
+        var invalidUuid = "";
+        var invalidName = "";
+        var validName = "Bruce Wayne";
+        var invalidDob = (LocalDate) null;
+        var validDob = LocalDate.of(1994,5,11);
+
+        assertThatThrownBy(() -> new Patient(uuid,invalidName,invalidDob))
+                .as("Name cannot be empty")
+                .isInstanceOf(AssertionError.class);
+
+        assertThatThrownBy(() -> new Patient(uuid,validName,invalidDob))
+                .as("Dob cannot be null")
+                .isInstanceOf(AssertionError.class);
+
+        assertThatThrownBy(() -> new Patient(invalidUuid,validName,validDob))
+                .as("Dob cannot be null")
+                .isInstanceOf(AssertionError.class);
+
+    }
+}
